5d3a0661020011d81d5453ca3165eddfb9302324
Jafar Azhar <jafar.azhar17@gmail.com>
2023-06-11 07:43:46 +0700

    styling image

--- src/components/Navigations.tsx
+++ src/components/Navigations.tsx
@@ -1,4 +1,14 @@
-import { Tabs, TabList, Tab, TabPanels, TabPanel, Box } from "@chakra-ui/react";
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Box,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -97,32 +107,127 @@ const Navigations = () => {
                 enabled: true,
               }}
               navigation={true}
+              style={{
+                // @ts-ignore comment
+                "--swiper-navigation-color": "black",
+              }}
               modules={[Keyboard, Navigation]}
-              className="mySwiper"
+              spaceBetween={24}
             >
               <SwiperSlide>
-                <img src="https://cdn.magloft.com/github/swiper/images/page-001.jpg" />
+                <Box
+                  position={"relative"}
+                  w={"432px"}
+                  h={"432px"}
+                  borderRadius={10}
+                  overflow={"hidden"}
+                  role="group"
+                >
+                  <Image
+                    src={
+                      "https://i.seadn.io/s/production/3e7c7b10-f282-445f-bfb9-c24b60a11288.png?auto=format&dpr=1&w=828"
+                    }
+                    alt="nav"
+                    boxSize={"max"}
+                    position={"absolute"}
+                    objectFit={"cover"}
+                    borderRadius={10}
+                    transition={"transform 0.5s ease"}
+                    _groupHover={{ transform: "scale(1.1)" }}
+                    zIndex={1}
+                  />
+                  <Stack zIndex={2}>
+                    <Text zIndex={2}>asdadasd</Text>
+                    <Text zIndex={2}>asdasd</Text>
+                  </Stack>
+                </Box>
               </SwiperSlide>
+
               <SwiperSlide>
-                <img src="https://cdn.magloft.com/github/swiper/images/page-002.jpg" />
+                <Box
+                  position={"relative"}
+                  w={"432px"}
+                  h={"432px"}
+                  borderRadius={10}
+                  overflow={"hidden"}
+                  role="group"
+                >
+                  <Image
+                    src={
+                      "https://i.seadn.io/s/production/3e7c7b10-f282-445f-bfb9-c24b60a11288.png?auto=format&dpr=1&w=828"
+                    }
+                    alt="nav"
+                    boxSize={"max"}
+                    position={"absolute"}
+                    objectFit={"cover"}
+                    borderRadius={10}
+                    transition={"transform 0.5s ease"}
+                    _groupHover={{ transform: "scale(1.1)" }}
+                    zIndex={1}
+                  />
+                  <Stack zIndex={2}>
+                    <Text zIndex={2}>asdadasd</Text>
+                    <Text zIndex={2}>asdasd</Text>
+                  </Stack>
+                </Box>
               </SwiperSlide>
+
               <SwiperSlide>
-                <img src="https://cdn.magloft.com/github/swiper/images/page-003.jpg" />
+                <Box
+                  position={"relative"}
+                  w={"432px"}
+                  h={"432px"}
+                  borderRadius={10}
+                  overflow={"hidden"}
+                  role="group"
+                >
+                  <Image
+                    src={
+                      "https://i.seadn.io/s/production/3e7c7b10-f282-445f-bfb9-c24b60a11288.png?auto=format&dpr=1&w=828"
+                    }
+                    alt="nav"
+                    boxSize={"max"}
+                    position={"absolute"}
+                    objectFit={"cover"}
+                    borderRadius={10}
+                    transition={"transform 0.5s ease"}
+                    _groupHover={{ transform: "scale(1.1)" }}
+                    zIndex={1}
+                  />
+                  <Stack zIndex={2}>
+                    <Text zIndex={2}>asdadasd</Text>
+                    <Text zIndex={2}>asdasd</Text>
+                  </Stack>
+                </Box>
               </SwiperSlide>
+
               <SwiperSlide>
-                <img src="https://cdn.magloft.com/github/swiper/images/page-004.jpg" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://cdn.magloft.com/github/swiper/images/page-005.jpg" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://cdn.magloft.com/github/swiper/images/page-006.jpg" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://cdn.magloft.com/github/swiper/images/page-007.jpg" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://cdn.magloft.com/github/swiper/images/page-008.jpg" />
+                <Box
+                  position={"relative"}
+                  w={"432px"}
+                  h={"432px"}
+                  borderRadius={10}
+                  overflow={"hidden"}
+                  role="group"
+                >
+                  <Image
+                    src={
+                      "https://i.seadn.io/s/production/3e7c7b10-f282-445f-bfb9-c24b60a11288.png?auto=format&dpr=1&w=828"
+                    }
+                    alt="nav"
+                    boxSize={"max"}
+                    position={"absolute"}
+                    objectFit={"cover"}
+                    borderRadius={10}
+                    transition={"transform 0.5s ease"}
+                    _groupHover={{ transform: "scale(1.1)" }}
+                    zIndex={1}
+                  />
+                  <Stack zIndex={2}>
+                    <Text zIndex={2}>asdadasd</Text>
+                    <Text zIndex={2}>asdasd</Text>
+                  </Stack>
+                </Box>
               </SwiperSlide>
             </Swiper>
           </TabPanel>
